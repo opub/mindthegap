b43e51b12c408df595ab5413a595f7a37494ec28
Ted O'Connor <ted@opub.com>
2021-03-18 09:53:28 -0400

    replaced smoothie with Apex charts

--- static/main.js
+++ static/main.js
@@ -6,8 +6,11 @@ socket.on('priceUpdate', function (data) {
         console.log(item.symbol, item);
         var chart = getChart(item.symbol);
         console.log('chart', chart);
-        chart.best.append(now, item.spreadPercent.best);
-        chart.short.append(now, item.spreadPercent.short);
+        chart.appendData([{
+            data: [[now, item.spreadPercent.best]]
+        }, {
+            data: [[now, item.spreadPercent.short]]
+        }]);
     }
 });
 
@@ -15,19 +18,110 @@ var charts = new Map();
 
 function getChart(symbol) {
     if (!charts.has(symbol)) {
+        var id = symbol.replace('/', '').toLowerCase();
         var wrapper = document.getElementById('wrapper');
-        var canvas = document.createElement('canvas');
-        canvas.id = 'chart-' + symbol.replace('/', '');
-        canvas.width = window.innerWidth;;
-        canvas.height = 200;
-        wrapper.appendChild(canvas);
+        var target = document.createElement('div');
+        target.id = 'target-' + id;
+        wrapper.appendChild(target);
 
-        var chart = new SmoothieChart({ tooltip: true, millisPerPixel: 1000, minValueScale: 1.05, maxValueScale: 1.05, title: { text: symbol, fontFamily: 'consolas', fontSize: 20, verticalAlign: 'top' }, grid: { strokeStyle: 'rgba(119,119,119,0.48)', millisPerLine: 40000, verticalSections: 4 },  labels: { fontFamily: 'consolas', fontSize: 16 } });
-        var best = new TimeSeries(), short = new TimeSeries();
-        chart.addTimeSeries(best, { lineWidth: 3, strokeStyle: '#00ff88' });
-        chart.addTimeSeries(short, { lineWidth: 3, strokeStyle: '#0088ff' });
-        chart.streamTo(canvas, 976);
-        charts.set(symbol, { chart, best, short });
+        var loader = document.getElementById('loader');
+        if(loader) {
+            loader.remove();
+        }
+
+        var options = {
+            series: [{ name: 'best', data: [] }, { name: 'short', data: [] }],
+            chart: {
+                id: 'chart-' + id,
+                height: 250,
+                type: 'line',
+                animations: {
+                    enabled: true,
+                    easing: 'linear',
+                    dynamicAnimation: {
+                        speed: 1000
+                    }
+                },
+                toolbar: {
+                    show: false
+                },
+                zoom: {
+                    enabled: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            title: {
+                text: symbol,
+                align: 'left'
+            },
+            markers: {
+                size: 1
+            },
+            xaxis: {
+                type: 'datetime'
+            },
+            legend: {
+                show: false
+            },
+        };
+
+        var chart = new ApexCharts(document.querySelector('#' + target.id), options);
+        chart.render();
+
+        charts.set(symbol, chart);
     }
     return charts.get(symbol);
 }
+
+window.Apex = {
+    chart: {
+      foreColor: '#fff',
+      toolbar: {
+        show: false
+      },
+    },
+    colors: ['#FCCF31', '#17ead9', '#f02fc2'],
+    stroke: {
+      width: 3
+    },
+    dataLabels: {
+      enabled: false
+    },
+    grid: {
+      borderColor: "#40475D",
+    },
+    xaxis: {
+      axisTicks: {
+        color: '#333'
+      },
+      axisBorder: {
+        color: "#333"
+      }
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        gradientToColors: ['#F55555', '#6078ea', '#6094ea']
+      },
+    },
+    tooltip: {
+      theme: 'dark',
+      x: {
+        formatter: function (val) {
+          return moment(new Date(val)).format("HH:mm:ss")
+        }
+      }
+    },
+    yaxis: {
+      decimalsInFloat: 2,
+      opposite: true,
+      labels: {
+        offsetX: -10
+      }
+    }
+  }
